f8c07608050d3871477470d04da9566761c48d31
Goodsie <gidsola@users.noreply.github.com>
2025-07-11 16:27:24 -0400

    Reimplement report system. Added documents/images

--- src/report.py
+++ src/report.py
@@ -1,8 +1,9 @@
 
 
 class Report:
-    def __init__(self, report_id, report_date):
+    def __init__(self, report_id, report_date, content=None):
         self.report_id = report_id
         self.report_date = report_date
+        self.content = content if content is not None else ""
         
     
--- src/reportManager.py
+++ src/reportManager.py
@@ -9,11 +9,9 @@ from src.report import Report
 class ReportManager:
     def __init__(self, user: 'User'):
         self.user = user
-        # self.report_id = report_id
-        # self.report_date = report_date
         
-    def generate_progress_report(self, report_id, report_date):
-        report = Report(report_id, report_date)
+    def generate_progress_report(self, report_id, report_date, content=None):
+        report = Report(report_id, report_date, content)
         data = [activity.calories_burned for activity in self.activities]
         report.generate_visual_representation(data)
         return report
@@ -36,7 +34,7 @@ class ReportManager:
 def showProgressReportMenu(user):
         report_id = 1
         report_date = input("Report date (YYYY-MM-DD): ")
-        report = Report(report_id=report_id, report_date=report_date)
+        report = Report(report_id=report_id, report_date=report_date, content="Progress report content")
         # user.view_progress_report(report)
         report.generate_visual_representation([1, 2, 3, 4, 5])
         input("Press Enter to continue...")
